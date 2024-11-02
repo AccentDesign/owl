f13ea9be1beca0b6e030ab4699ecb3c199328491
Stuart George <stuart@accentdesign.co.uk>
2024-11-02 22:02:21 +0000

    add submit to form

--- examples/form_templ.go
+++ examples/form_templ.go
@@ -43,11 +43,33 @@ func Form() templ.Component {
 				}()
 			}
 			ctx = templ.InitializeContext(ctx)
-			templ_7745c5c3_Err = css.Include("form_field.css", "label.css", "checkbox.css", "input.css", "select.css", "textarea.css").Render(ctx, templ_7745c5c3_Buffer)
+			templ_7745c5c3_Err = css.Include("form_field.css", "label.css", "checkbox.css", "input.css", "select.css", "textarea.css", "button.css").Render(ctx, templ_7745c5c3_Buffer)
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(" <h2 class=\"owl-h2 mb-10\">Form</h2><form class=\"space-y-10 max-w-[500px]\"><div class=\"owl-form-field\"><label class=\"owl-label\" for=\"email\">Email</label> <input class=\"owl-input\" id=\"email\" name=\"email\" type=\"email\" placeholder=\"owl text\" value=\"someone@example.com\"><p class=\"owl-form-field-description\">This is your email address.</p></div><div class=\"owl-form-field\"><label class=\"owl-label\" for=\"password\">Password</label> <input class=\"owl-input\" id=\"password\" name=\"password\" type=\"password\" placeholder=\"owl password\"><p class=\"owl-form-field-error\">Invalid username of password.</p></div><div class=\"owl-form-field\"><label class=\"owl-label\" for=\"sex\">Sex</label> <select class=\"owl-select\" id=\"sex\" name=\"sex\"><option>Please select..</option> <option value=\"Male\">Male</option> <option value=\"Female\">Female</option></select><p class=\"owl-form-field-description\">This is your sex.</p></div><div class=\"owl-form-field\"><label class=\"owl-label\" for=\"comments\">Comments</label> <textarea class=\"owl-textarea\" id=\"comments\" name=\"comments\" placeholder=\"owl comments\"></textarea><p class=\"owl-form-field-description\">Leave us some comments.</p></div><div class=\"owl-form-field\"><label class=\"owl-label\" for=\"file\">File</label> <input class=\"owl-input\" id=\"file\" name=\"file\" type=\"file\"><p class=\"owl-form-field-description\">Add some random file.</p></div><div class=\"owl-form-field\"><label class=\"owl-label flex items-center gap-2\" for=\"agree\"><input class=\"owl-checkbox\" id=\"agree\" name=\"agree\" type=\"checkbox\"> Terms and conditions</label><p class=\"owl-form-field-description\">Please agree to the terms.</p></div></form>")
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(" <h2 class=\"owl-h2 mb-10\">Form</h2><form class=\"space-y-10 max-w-[500px]\"><div class=\"owl-form-field\"><label class=\"owl-label\" for=\"email\">Email</label> <input class=\"owl-input\" id=\"email\" name=\"email\" type=\"email\" placeholder=\"owl text\" value=\"someone@example.com\"><p class=\"owl-form-field-description\">This is your email address.</p></div><div class=\"owl-form-field\"><label class=\"owl-label\" for=\"password\">Password</label> <input class=\"owl-input\" id=\"password\" name=\"password\" type=\"password\" placeholder=\"owl password\"><p class=\"owl-form-field-error\">Invalid username of password.</p></div><div class=\"owl-form-field\"><label class=\"owl-label\" for=\"sex\">Sex</label> <select class=\"owl-select\" id=\"sex\" name=\"sex\"><option value=\"\">Please select..</option> <option value=\"Male\">Male</option> <option value=\"Female\">Female</option></select><p class=\"owl-form-field-description\">This is your sex.</p></div><div class=\"owl-form-field\"><label class=\"owl-label\" for=\"comments\">Comments</label> <textarea class=\"owl-textarea\" id=\"comments\" name=\"comments\" placeholder=\"owl comments\"></textarea><p class=\"owl-form-field-description\">Leave us some comments.</p></div><div class=\"owl-form-field\"><label class=\"owl-label\" for=\"file\">File</label> <input class=\"owl-input\" id=\"file\" name=\"file\" type=\"file\"><p class=\"owl-form-field-description\">Add some random file.</p></div><div class=\"owl-form-field\"><label class=\"owl-label flex items-center gap-2\" for=\"agree\"><input class=\"owl-checkbox\" id=\"agree\" name=\"agree\" type=\"checkbox\"> Terms and conditions</label><p class=\"owl-form-field-description\">Please agree to the terms.</p></div><div>")
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			var templ_7745c5c3_Var3 = []any{ButtonDefault}
+			templ_7745c5c3_Err = templ.RenderCSSItems(ctx, templ_7745c5c3_Buffer, templ_7745c5c3_Var3...)
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<button class=\"")
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			var templ_7745c5c3_Var4 string
+			templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(templ.CSSClasses(templ_7745c5c3_Var3).String())
+			if templ_7745c5c3_Err != nil {
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `examples/form.templ`, Line: 1, Col: 0}
+			}
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\">Submit</button></div></form>")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
