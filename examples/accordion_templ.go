f82fbd266dc2afbdc798beeb7b8cc4400cce15f6
Stuart George <stuart@accentdesign.co.uk>
2024-11-02 21:15:43 +0000

    use owl-accordion-trigger-icon class

--- examples/accordion_templ.go
+++ examples/accordion_templ.go
@@ -87,7 +87,7 @@ func Accordion() templ.Component {
 				if templ_7745c5c3_Err != nil {
 					return templ_7745c5c3_Err
 				}
-				templ_7745c5c3_Err = icons.ChevronDown("size-6").Render(ctx, templ_7745c5c3_Buffer)
+				templ_7745c5c3_Err = icons.ChevronDown("owl-accordion-trigger-icon").Render(ctx, templ_7745c5c3_Buffer)
 				if templ_7745c5c3_Err != nil {
 					return templ_7745c5c3_Err
 				}
